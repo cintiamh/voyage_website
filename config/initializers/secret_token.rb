38b2ae156b0463c938e44091ef54ca57ed7cee13
Cintia Higashi <cintiamh@gmail.com>
2013-01-31 15:14:36 -0500

    Created project with Bootstrap and Active Admin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Voyage::Application.config.secret_token = 'c43bb9c0a51b7921ee0023f6d7b128efda73df0c9dc049b4caef8524ae4dd3c9f788043397af385e9b08a7171e74ff7b0b88c7a6966ab59394a18823345d172d'
